ca3c799a3359ddd74ce73e755e7dcb9b4a48c18a
v1gnesh <v1gnesh@users.noreply.github.com>
2024-01-20 07:19:08 +0530

    Update cicd-stable.groovy

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/NATSport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/NATSport.git'), string(name: 'PORT_DESCRIPTION', value: 'NATS is a simple, secure and performant communications system for digital systems, services and devices.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/NATSport.git'), string(name: 'PORT_DESCRIPTION', value: 'NATS is a simple, secure and performant communications system for digital systems, services and devices.' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "go_120" ) ]
   }
 }
